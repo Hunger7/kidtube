a3b9a1c0ead4a78f562163b1d8c8a3b15b733fd1
Hunger7 <info@inbox33.us>
2026-09-10 10:33:15 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL = "shelf-shell-v3";
+const SHELL = "shelf-shell-v4";
 const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", e => {
